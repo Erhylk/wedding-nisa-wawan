59eccb24f341234ac6934bbc4460f70550c280c8
Erhylk <erhylksym@gmail.com>
2025-12-29 12:25:21 +0800

    update form

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.jsx";
+import "./index.css";
 
 const queryClient = new QueryClient();
-<QueryClientProvider client={queryClient}>
-  <app />
-</QueryClientProvider>
 
 createRoot(document.getElementById("invito-layout")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
+
